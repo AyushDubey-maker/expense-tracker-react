95f6c8942d07bf827a50b7eb7e989b9854b47f3e
Ayush <ayushdubey2611@gmail.com>
2021-05-11 11:14:04 +0530

    Some Styling changes

--- expense-tracker/src/components/AddTransaction.js
+++ expense-tracker/src/components/AddTransaction.js
@@ -29,7 +29,7 @@ function AddTransaction() {
       <form onSubmit={submitTransaction}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text..." />
+          <input required type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
